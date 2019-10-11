c6f8a177471d978ef8abf4117e9cbbc0c7a44a15
umarisyraf <56431314+umarisyraf@users.noreply.github.com>
2019-10-11 17:07:18 +0800

    Add files via upload

---
+++ test1.php
@@ -0,0 +1,18 @@
+<?php
+	
+	$favcolor = "red";
+	switch($favcolor){
+		case"red" :
+			echo"Your favorite color is red!";
+			break;
+		case"blue" :
+			echo"Your favorite color is red!";
+			break;
+		case"green" :
+			echo"Your favorite color is red!";
+			break;
+		default:
+			echo"Your don't have favorite color";
+ 
+	}
+?>
